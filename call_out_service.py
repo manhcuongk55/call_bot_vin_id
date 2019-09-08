b5d79b872c404df7c33b99ef6aaabcfb120528eb
manhcuongk55 <dmcksclck55@gmail.com>
2019-09-08 11:45:08 +0700

    Add code

---
+++ call_out_service.py
@@ -0,0 +1,32 @@
+from flask_cors import CORS
+from flask import Flask, request, json, jsonify, send_file
+import os
+import json
+import logging
+logging.getLogger().setLevel(logging.INFO)
+
+import config
+from pyfcm import FCMNotification
+app = Flask(__name__)
+CORS(app)
+FCM_APIKEY = 'AAAAMDfbpUs:APA91bFx3HK2UvyhzwZLEad_9N5J8leH5dK81uzzJJVt7oXHbNrKgdtryLbjp8bc4yP5MhSpH5xG0OPn6LfRKOBCAqDo53G8K1xGnLQ9fgnAQV_hrLT_1gKwlnOHTtl9gCC6XISNv7Rk'
+def call2clients(registration_ids):
+    push_service = FCMNotification(api_key=FCM_APIKEY)
+    result = push_service.notify_multiple_devices(
+        registration_ids=registration_ids,
+        data_message={'phoneNumber': '18003333'})
+    print(result)
+
+# print(result)
+
+@app.route("/pizza/call", methods=["POST"])
+def call():
+    if request.method == 'POST':
+        registration_ids = ['eIKn6CD8Le4:APA91bH-JTx4czZH2kouyiiinjV3EcnbBjBX2EdDdNwdlHYKq1M9zUkTIKckYqoz6flhdil-r3eovb-uhZiD9ZU8MPSJuie4dM2i_kVxlD7Gr_vHWy4Sa0aYSfIbs8ViWhWsufoP64xk']
+        call2clients(registration_ids)
+    return json.dumps({
+        'error_code': 0,
+        'data_message':'Ok'
+    })    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=6789)
---
+++ test_client.py
@@ -0,0 +1,71 @@
+from __future__ import print_function
+import sys
+import traceback
+import logging
+import pyaudio
+import _thread
+logging.basicConfig(format='%(asctime)s\t%(levelname)s\t%(message)s', level=logging.INFO)
+
+import grpc
+import time
+
+import voicebot_pb2
+import voicebot_pb2_grpc
+
+CHUNK = 4000
+CHANNELS = 1
+RATE = 16000
+CONNECTED = False
+
+def record_block():
+    global CONNECTED
+    yield voicebot_pb2.VoiceBotRequest(
+        voicebot_config=voicebot_pb2.VoiceBotConfig(
+            call_center_code='18005555'))
+    p = pyaudio.PyAudio()
+    stream = p.open(format=pyaudio.paInt16,channels=CHANNELS,rate=RATE,input=True,frames_per_buffer=CHUNK)
+    while True:
+        block = stream.read(CHUNK)
+        block_audio = voicebot_pb2.VoiceBotRequest(audio_content=block)
+        if CONNECTED:
+            yield block_audio
+
+
+def play_audio(url):
+    sys.stdout.write('TTS url: {}\n'.format(url))
+    sys.stdout.flush()
+
+def run():
+    global CONNECTED
+    # NOTE(gRPC Python Team): .close() is possible on a channel and should be
+    # used in circumstances in which the with statement does not fit the needs
+    # of the code.
+    # with grpc.insecure_channel('123.31.18.120:50051') as channel:
+    with grpc.insecure_channel('localhost:50051') as channel:
+        voicebot_stub = voicebot_pb2_grpc.VoiceBotStub(channel)
+
+        list_call_center_response = voicebot_stub.ListCallCenter(voicebot_pb2.ListCallCenterRequest())
+        for call_center in list_call_center_response.call_centers:
+            print(call_center.code, call_center.name)
+
+        voicebot_response_iterator = voicebot_stub.CallToBot(record_block())
+        voicebot_response = next(voicebot_response_iterator)
+        if voicebot_response.status.code == 200:
+            logging.info("Connected to call center")
+            logging.info("Speak something ...")
+            CONNECTED = True
+            for voicebot_response in voicebot_response_iterator:
+                if not voicebot_response.final:
+                    sys.stdout.write('\rMe: {}'.format(voicebot_response.text_asr))
+                    sys.stdout.flush()
+                else:
+                    sys.stdout.write('\rMe: {}\n'.format(voicebot_response.text_asr))
+                    sys.stdout.write('Bot: {}\n'.format(voicebot_response.text))
+                    sys.stdout.flush()
+                    if voicebot_response.audio_url is not None:
+                        _thread.start_new_thread(play_audio, (voicebot_response.audio_url, ))
+        else:
+            logging.error("{} - {}".format(voicebot_response.status.code, voicebot_response.status.message))
+
+if __name__ == '__main__':
+    run()
